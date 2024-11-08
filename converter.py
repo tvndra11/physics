79dfc5fd023ff9b3962417b326268409fc3ab025
tvndra11 <128711464+tvndra11@users.noreply.github.com>
2024-11-08 13:51:21 +0000

    added all up to the end of aqa unit 6

--- converter.py
+++ converter.py
@@ -4,6 +4,48 @@ def GravitationalPotential(Mass,Height,Grav):
 def KineticEnergy(Mass,Velocity):
     KE = Mass*Velocity*Velocity*0.5
     return(KE)
-def Power(Time,Power):
+def PowerPT(Time,Power):
     P = Power/Time
     return(P)
+def ElasticPotential(SpringConstant,Extension):
+    EP = SpringConstant*Extension*Extension*0.5
+    return(EP)
+def ChangeInThermalEnergy(Mass,SHC,Change):
+    CITE = Mass*SHC*Change
+    return(CITE)
+def ChargeFlow(Current,Time):
+    CF = Current*Time
+    return(CF)
+def PotentialDifference(Current,Resistance):
+    PD = Current*Resistance
+    return(PD)
+def PowerCPD(Current,PotentialDifference):
+    P = Current*PotentialDifference
+    return(P)
+def PowerCR(Current,Resistance):
+    P = Current*Current*Resistance
+    return(P)
+def EnergyTransferedPT(Power,Time):
+    ET = Power*Time
+    return(ET)
+def EnergyTransferedCFPD(Current,PotentialDifference):
+    ET = Current*PotentialDifference
+    return(ET)
+def Density(Volume,Mass):
+    D = Volume*Mass
+    return(D)
+def ThermalEnergyForAChangeInState(Mass,SpecificLatentHeat):
+    TEFACIS = Mass*SpecificLatentHeat
+    return(TEFACIS)
+def GasPressureConstant(Pressure,Volume):
+    GPC = Pressure*Volume
+    return(GPC)
+def WaveSpeed(WaveLength,Frequency):
+    WS = WaveLength*Frequency
+    return(WS)
+def TimePeriod(Frequency):
+    TP = 1/Frequency
+    return(TP)
+def Magnification(ImageHeight,ObjectHeight):
+    M = ImageHeight/ObjectHeight
+    return(M)
